e974ee1d7cc444dbd68a09c1e34a033abdc8b7c8
Nath <nathanael.dijouxpro@gmail.com>
2022-06-21 16:05:04 +0200

    Step-3 /Patch 2 : Elements integration

--- front/js/script.js
+++ front/js/script.js
@@ -1,14 +1,30 @@
-function retrieveDataApi() {
-    fetch("http://localhost:3000/api/products")
+fetch("http://localhost:3000/api/products")
     .then(function(res) {
       if (res.ok) {
         return res.json();
       }
     })
-    .then(function(value) {
-      console.log(value);
+    .then(function(products) {
+      products.forEach (product =>{
+        let items = document.getElementById ("items");
+        let article = document.createElement ("article");
+        let img = document.createElement ("img");
+        let alt = document.createElement ("alt")
+        img.innerHTML = product.imageUrl;
+        alt.innerHTML = product.altTxt;
+        let h3 = document.createElement ("h3");
+        h3.classList.add("productName");
+        h3.innerHTML = product.name;
+        let p = document.createElement ("p");
+        p.classList.add("productDescription");
+        p.innerHTML = product.description;
+        items.appendChild(article);
+        article.appendChild(h3);
+        article.appendChild(img);
+        article.appendChild(alt);
+        article.appendChild(p);
+
+      })
     })
-    .catch(function(err) {
-      // Une erreur est survenue
-    });
-  }
+    
+  
