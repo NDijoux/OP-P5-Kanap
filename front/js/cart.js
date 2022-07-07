626cf03af537941f0c57cc4749acef590860fed9
Nath <nathanael.dijouxpro@gmail.com>
2022-07-07 17:50:48 +0200

    Step - 8 / Patch 2 : Display products with key elements

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,21 +1,31 @@
 // Data recovery from API & Localstorage ---------------------------
 
 let cartData = JSON.parse(localStorage.getItem("itemForCart"));
-
-fetch("http://localhost:3000/api/products/")
+function displayCart () {
+  for (let kanap of cartData) {
+    let retrieveDataItems = 
+    {
+    id : kanap.id,
+    color : kanap.color,
+    quantity : kanap.quantity,
+    }
+   fetch("http://localhost:3000/api/products/"+retrieveDataItems.id)
     .then(function(res) {
-      if (res.ok) {
+        if (res.ok) {
         return res.json();
       }
     })
 // Function to display the Cart & the Products in it -------------------
     .then (function (cartProducts){
         let cartSection = document.querySelector("#cart__items");
-        cartSection.innerHTML = 
-        `<article class="cart__item" data-id="${cartProducts._id}" data-color="${cartProducts.color}"></article>`;
+        let article = document.createElement ("article");
+        cartSection.appendChild(article);
+        article.classList.add('cart__item');
+        article.setAttribute('data-id',retrieveDataItems.id);
+        article.setAttribute('data-color',retrieveDataItems.color);
         let cartImg = document.createElement ("div");
         cartImg.classList.add('cart__item__img');
-        cartSection.appendChild(cartImg)
+        cartSection.appendChild(cartImg);
         let img = document.createElement ("img");
         img.src = cartProducts.imageUrl;
         img.alt = cartProducts.altTxt;
@@ -30,7 +40,7 @@ fetch("http://localhost:3000/api/products/")
         cartItemName.innerHTML = cartProducts.name;
         cartDescription.appendChild(cartItemName);
         let cartItemColor = document.createElement ("p");
-        cartItemColor.innerHTML = cartProducts.color; 
+        cartItemColor.innerHTML = retrieveDataItems.color; 
         cartDescription.appendChild(cartItemColor);
         let cartItemPrice = document.createElement ("p");
         cartItemPrice.innerHTML = cartProducts.price;
@@ -38,7 +48,24 @@ fetch("http://localhost:3000/api/products/")
         let cartSettings = document.createElement ("div");
         cartSettings.classList.add('cart__item__content__settings');
         cartContent.appendChild(cartSettings);
-
+        let cartQuantity = document.createElement ("div");
+        cartQuantity.classList.add('cart__item__content__settings__quantity');
+        cartSettings.appendChild(cartQuantity);
+        let cartQuantityTxt = document.createElement ("p");
+        cartQuantityTxt.textContent = "Qté :";
+        cartQuantity.appendChild(cartQuantityTxt);
+        let cartQuantityInput = document.createElement ("input");
+        cartQuantityInput.type = "number";
+        cartQuantityInput.className = "itemQuantity";
+        cartQuantityInput.name = "itemQuantity";
+        cartQuantityInput.min = "1";
+        cartQuantityInput.max = "100";
+        cartQuantityInput.value = retrieveDataItems.quantity
+        cartQuantity.appendChild(cartQuantityInput);
+    })
+   }
+  }
+    displayCart();
 // Function to manage quantity --------------------------------------------------------------------
 
         /* let cartQuantity = document.createElement ("div");
@@ -52,7 +79,7 @@ fetch("http://localhost:3000/api/products/")
         cartQuantityInput.type = "number";
         cartQuantityInput.className = "itemQuantity";
         cartQuantityInput.name = "itemQuantity";
-        cartQuantity.min = "1";
+        cartQuantityInput.min = "1";
         cartQuantityInput.max = "100";
         cartQuantityInput.value = "42";
         cartQuantity.appendChild(cartQuantityInput);
@@ -68,4 +95,3 @@ fetch("http://localhost:3000/api/products/")
         cartDelete.appendChild(cartDeleteTxt); */
 // Function to calculate and display total quantity
 // Function to calculate and display total price
-    })
